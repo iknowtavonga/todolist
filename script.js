e001f65d3dbf9542bec98d30704ad19be2f33275
Tavonga <67345561+iknowtavonga@users.noreply.github.com>
2022-03-30 21:25:03 +0200

    adding a new todo

--- script.js
+++ script.js
@@ -7,12 +7,18 @@ const toDoArray = ['wake up', 'drink some coffee'];
 while (userInput !== 'quit' && userInput !== 'q') {
     if (userInput === 'list') {
         console.log('************');
+        // after seperating we will loop through the array to list all the contents
         for (let i = 0; i < toDoArray.length; i++) {
             console.log(`${i}: ${toDoArray[i]}`)
         }
         console.log('************');
-
-    }/*
+    } else if (input === 'new') {
+        // we create a new prompt that will take the new todo and store it for later use
+        const newTodo = prompt('What is your new todo?');
+        toDoArray.push(newTodo);
+        console.log(`${newTodo} added to the list!`)
+    }
+    /*
     if (userInput === 'new') {
         let newItem = prompt('Enter new todo');
         toDoArray.push(newItem);
